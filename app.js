db7fa0650d4e33967565942ac6fe7286cd9c4213
Dominique <dfoleydev@gmail.com>
2022-06-13 21:59:21 -0400

    Added express and ejs.

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const PORT = 4020
 
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.listen(PORT, () => {
