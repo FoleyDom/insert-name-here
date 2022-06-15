65860bb652513b0d7ef43eb38da3eee26c031ad7
Dominique <dfoleydev@gmail.com>
2022-06-14 22:27:02 -0400

    Using bootstrap

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const PORT = 4020
 
 app.set('view engine', 'ejs')
+
+app.use(
+    '/css',
+    express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')),
+)
+app.use(
+    '/js',
+    express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')),
+)
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
 app.use(express.static('public'))
 
